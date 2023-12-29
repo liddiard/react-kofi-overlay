d3660a396e4cbefb3c0661f142f1680ee463dd8b
Harrison Liddiard <omniaura5@gmail.com>
2023-12-28 21:52:45 -0800

    fix webpack config - fix global object ("'self' is not defined") - fix import on nextjs app (must be a client component for useState) by inserting 'use client' at top of bundle - include styles in bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const webpack = require('webpack')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
   entry: './src/index.tsx',
-  mode: 'none',
+  mode: 'production',
   module: {
     rules: [
       {
@@ -20,13 +21,34 @@ module.exports = {
         },
       },
       {
-        test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
       },
     ],
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            // preserve "use client" directive (inserted below w/ BannerPlugin)
+            // https://github.com/terser/terser?tab=readme-ov-file#compress-options
+            directives: false
+          }
+        }
+      }),
+    ],
   },
   output: {
     filename: 'bundle.js',
@@ -34,8 +56,20 @@ module.exports = {
     libraryTarget: 'umd',
     library: 'KofiDonate',
     umdNamedDefine: true,
+    globalObject: 'this'
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    // Inject a "use client" at the top of the bundle so frameworks like
+    // Next.js see it as a boundary between client and server.
+    // This feels a little hacky so hopefully this can be improved with better
+    // webpack support in the future.
+    new webpack.BannerPlugin({
+      // https://webpack.js.org/plugins/banner-plugin/#options
+      banner: "'use client';",
+      raw: true,
+      include: [/\.(ts|tsx|js)$/]
+    }),
+  ],
   externals: {
     react: 'react',
     'react-dom': 'react-dom',
